465c63ed7c54a3f758f3ae598f21e89bb86328d3
yu-yang2 <youngyuhpe@gmail.com>
2016-05-29 21:28:01 +0800

    Initial commit

---
+++ app/src/main/java/com/yyang2/demo_databasetest/MyDatabaseHelper.java
@@ -0,0 +1,46 @@
+package com.yyang2.demo_databasetest;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+/**
+ * Created by yyang2 on 5/29/2016.
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String CREATE_BOOK = "create table book ("
+            + "id integer primary key autoincrement,"
+            + "author text,"
+            + "price real,"
+            + "pages integer,"
+            + "name text)";
+
+    public static final String CREATE_CATEGORY = "create table Category ("
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)";
+
+    private Context mContext;
+
+    public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_BOOK);
+        db.execSQL(CREATE_CATEGORY);
+        Toast.makeText(mContext, "create succeeded", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int vewVersion) {
+        db.execSQL("drop table if exists Book");
+        db.execSQL("drop table if exists Category");
+        onCreate(db);
+    }
+
+}
